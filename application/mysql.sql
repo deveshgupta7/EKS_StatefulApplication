f9a7448ebd8a6bd20d84eec56b86751ed486331d
deveshgupta7 <contact.devesh37@gmail.com>
2024-12-04 21:19:56 +0000

    new changes

--- application/mysql.sql
+++ application/mysql.sql
@@ -8,8 +8,8 @@ last_name VARCHAR(20),
 primary_skill VARCHAR(20),
 location VARCHAR(20));
 
-INSERT INTO employee VALUES ('1','Allana','Malik','Laugh','Orignal');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('1','Devesh','Gupta','Laugh','Orignal');
+INSERT INTO employee VALUES ('1','Varun','Pal','Empathy','alien');
 SELECT * FROM employee;
 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
